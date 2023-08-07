5df25fd69414905db001cbb263c91d801b205b98
rsmith1631 <rsmith1631@gmail.com>
2023-08-06 18:35:37 -0700

    TacoParser

--- LoggingKata.Test/TacoParserTests.cs
+++ LoggingKata.Test/TacoParserTests.cs
@@ -9,15 +9,13 @@ namespace LoggingKata.Test
         [Fact]
         public void ShouldDoSomething()
         {
-            // TODO: Complete Something, if anything
-
-            //Arrange
+            
             var tacoParser = new TacoParser();
 
-            //Act
+            
             var actual = tacoParser.Parse("34.073638, -84.677017, Taco Bell Acwort...");
 
-            //Assert
+            
             Assert.NotNull(actual);
 
         }
@@ -26,20 +24,26 @@ namespace LoggingKata.Test
         [InlineData("34.073638, -84.677017, Taco Bell Acwort...", -84.677017)]
         public void ShouldParseLongitude(string line, double expected)
         {
-            // TODO: Complete - "line" represents input data we will Parse to
-            //       extract the Longitude.  Your .csv file will have many of these lines,
-            //       each representing a TacoBell location
-
-            //Arrange
+            
             var tacoParser = new TacoParser();
-            //Act
+           
             var actual = tacoParser.Parse(line);
-            //Assert
+            
             Assert.Equal(expected, actual.Location.Longitude);
         }
 
 
-        //TODO: Create a test ShouldParseLatitude
-
+        
+        [Theory]
+        [InlineData("34.073638, -84.677017, Taco Bell Acwort...", 34.073638)]
+        public void ShouldParseLatitude(string line, double expected)
+        {
+          
+            var tacoParser = new TacoParser();
+            
+            var actual = tacoParser.Parse(line);
+            
+            Assert.Equal(expected, actual.Location.Latitude);
+        }
     }
 }
--- LoggingKata/TacoBell.cs
+++ LoggingKata/TacoBell.cs
@@ -6,7 +6,9 @@ using System.Threading.Tasks;
 
 namespace LoggingKata
 {
-    internal class TacoBell
+    public class TacoBell : ITrackable
     {
+        public string Name { get; set; }
+        public Point Location { get; set; }
     }
 }
